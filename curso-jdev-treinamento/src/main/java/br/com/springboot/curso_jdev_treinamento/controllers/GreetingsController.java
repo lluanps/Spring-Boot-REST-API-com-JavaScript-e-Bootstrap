c2a9f643ad53ec454b1563eacc512240df15db08
Luan Pinheiro <lluanps@gmail.com>
2023-01-27 22:55:47 -0300

    Criado repository, model e controller

---
+++ curso-jdev-treinamento/src/main/java/br/com/springboot/curso_jdev_treinamento/controllers/GreetingsController.java
@@ -0,0 +1,30 @@
+package br.com.springboot.curso_jdev_treinamento.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.springboot.curso_jdev_treinamento.repository.UsuarioRepository;
+
+@RestController
+public class GreetingsController {
+
+	@Autowired
+	private UsuarioRepository usuarioRepository;
+	
+    @RequestMapping(value = "/{name}", method = RequestMethod.GET)
+    @ResponseStatus(HttpStatus.OK)
+    public String greetingText(@PathVariable String name) {
+        return "Hello " + name + "!";
+    }
+    
+    @RequestMapping(value = "/olamundo/{nome}", method = RequestMethod.GET)
+    @ResponseStatus(HttpStatus.OK)
+    public String metodoOlaMundo(@PathVariable String nome)  {
+    	return "Ola mundo " + nome;
+    }
+}
